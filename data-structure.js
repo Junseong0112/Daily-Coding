493d62eed8413985f97368f826712f4c652614ad
Jun Seong <drawbeat0112@gmail.com>
2023-07-12 18:01:27 +0900

    data-structure update

--- data-structure.js
+++ data-structure.js
@@ -111,25 +111,58 @@ const isValid = (str) => {
 
 function paveBox(boxes) {
   let answer = [];
+  
+  // boxes 배열이 0보다 클 때까지 반복합니다.
+  while(boxes.length > 0){
+      let finishIndex = boxes.findIndex(fn => boxes[0] < fn);
+      
+      if(finishIndex === -1){
+          // 만약 찾지 못했다면 answer에 boxes 배열의 길이를 넣은 후, boxes 내부의 요소를 전부 삭제합니다.
+          answer.push(boxes.length);
+          boxes.splice(0, boxes.length);
 
-  for (let i = 0; i < boxes.length; i++) {
-    let finishIndex = -1;
-
-    for (let j = 1; j < boxes.length; j++) {
-      if (boxes[0] < boxes[j]) {
-        finishIndex = j;
-        break;
+      } else {
+          // 만약 찾았다면, 해당 인덱스를 answer에 넣고, boxes에서 그만큼 제외합니다.
+          answer.push(finishIndex);
+          boxes.splice(0, finishIndex);
       }
-    }
-
-    if (finishIndex === -1) {
-      answer.push(boxes.length);
-      boxes.splice(0, boxes.length);
-    } else {
-      answer.push(finishIndex);
-      boxes.splice(0, finishIndex);
-    }
   }
 
+  // 결과물을 반환합니다.
   return Math.max(...answer);
+}
+
+// 05_[Tree] 구현
+
+class Tree {
+  constructor(value) {
+		// constructor로 만든 객체는 트리의 Node가 됩니다.
+    this.value = value;
+    this.children = [];
+  }
+
+	// 트리의 삽입 메서드를 만듭니다.
+  insertNode(value) {
+		// 값이 어떤 이름으로 만들어지고 어느 위치에 붙는지 떠올리는 것이 중요합니다.
+		// TODO: 트리에 붙게 될 childNode를 만들고, children에 넣어야 합니다.
+    const childNode = new Tree(value);
+    this.children.push(childNode);
+  }
+
+	// 트리 안에 해당 값이 포함되어 있는지 확인하는 메서드를 만듭니다.
+  contains(value) {
+		// TODO: 값이 포함되어 있다면 true를 반환하세요. 
+    if (this.value === value) {
+      return true;
+    }
+		// TODO: 값을 찾을 때까지 children 배열을 순회하며 childNode를 탐색하세요.
+    for (let i = 0; i < this.children.length; i += 1) {
+      const childNode = this.children[i];
+      if (childNode.contains(value)) {
+        return true;
+      }
+    }
+		// 전부 탐색했음에도 불구하고 찾지 못했다면 false를 반환합니다.
+    return false;
+  }
 }
