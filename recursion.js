d2b7abf625d6d2d9b0b3a269c41547b7be8adcf6
Jun Seong <drawbeat0112@gmail.com>
2023-07-29 21:21:21 +0900

    no.6 type done

--- recursion.js
+++ recursion.js
@@ -58,5 +58,16 @@ function arrSum(arr) {
   return head + arrSum(tail);
 }
 
+// 6.배열을 입력받아 모든 요소의 곱을 리턴해야 합니다.
+function arrProduct(arr) {
+  if (arr.length === 0) {
+    return 1;
+  }
+
+  // const [head, ...tail] = arr;
+  const head = arr[0];
+  const tail = arr.slice(1);
+  return head * arrProduct(tail);
+}
 
 
