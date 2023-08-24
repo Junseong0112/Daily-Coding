9ed255b302d54eb7500461dc454c3dc7842715e9
Junseong <108931006+Junseong0112@users.noreply.github.com>
2023-08-24 09:40:46 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,19 @@
+// function solution(num1, num2) {
+//     let ture = 1;
+//     if(num1 == num2){
+//         return ture;
+//     }else{
+//         ture = -1
+//         return ture;
+//     }
+// }
+
+function solution(num1, num2) {
+    let count = 1;
+    if(num1 == num2){
+        return count;
+    }else{
+        count = -1
+        return count;
+    }
+}
