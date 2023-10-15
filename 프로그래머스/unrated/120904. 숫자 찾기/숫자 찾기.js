d84d30fca40701897b39d63cd92845e54c602dd7
Junseong <108931006+Junseong0112@users.noreply.github.com>
2023-10-15 10:04:26 +0900

    [level unrated] Title: 숫자 찾기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.js
@@ -0,0 +1,8 @@
+function solution(num, k) {
+    let result = String(num)
+    result = [...result].map((el) => Number(el)).findIndex((el) => el === k)
+    if(result !== -1){
+        result += 1
+    }
+    return result
+}
