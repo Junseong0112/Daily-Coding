645ec6a65700a3f9de4dbccca5e6dc1ea2ef7b46
Junseong <108931006+Junseong0112@users.noreply.github.com>
2023-10-31 20:04:31 +0900

    [level unrated] Title: 개미 군단, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
@@ -1,3 +1,5 @@
 function solution(hp) {
-      return ~~(hp / 5) + ~~((hp % 5) / 3) + ~~((hp % 5) % 3);
+    // return Math.floor(hp/5)+Math.floor((hp%5)/3)+(hp%5)%3;
+     
+    return ~~(hp / 5) + ~~((hp % 5) / 3) + ~~((hp % 5) % 3);
 }
