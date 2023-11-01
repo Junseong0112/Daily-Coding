f4ef34b57ec0adbc3a50371c5abe5c9050b46256
Junseong <108931006+Junseong0112@users.noreply.github.com>
2023-11-01 22:59:20 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,5 +1,9 @@
 function solution(my_string, num1, num2) {
-    const arr = [...my_string]
-    const result = [arr[num1], arr[num2]] = [arr[num2], arr[num1]]
-    return arr.join('')
+    // const arr = [...my_string]
+    // const result = [arr[num1], arr[num2]] = [arr[num2], arr[num1]]
+    // return arr.join('')
+    
+    let result = my_string.split('');
+    [result[num1] , result[num2]] = [result[num2] , result[num1]];
+    return result.join('');
 }
