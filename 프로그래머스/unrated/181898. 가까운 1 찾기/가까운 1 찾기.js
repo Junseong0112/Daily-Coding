b79ef74e789c70ba54adca81441ab6705f9040de
Junseong <108931006+Junseong0112@users.noreply.github.com>
2023-11-01 14:27:11 +0000

    Update 가까운 1 찾기.js

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,4 +1,6 @@
 function solution(arr, idx) {
-  // return arr.slice(idx).indexOf(1) === -1 ? -1 : arr.indexOf(1, idx)
-  return arr.indexOf(1, idx)
+  return arr.slice(idx).indexOf(1) === -1 ? -1 : arr.indexOf(1, idx)
+
+  //다른 사람의 풀이..
+  //return arr.indexOf(1, idx)
 }
