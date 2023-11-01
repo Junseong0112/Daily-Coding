da6058bca1c07709adc33d55985aaa9f60faf3aa
Junseong <108931006+Junseong0112@users.noreply.github.com>
2023-11-01 23:25:43 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,3 +1,4 @@
 function solution(arr, idx) {
-  return arr.slice(idx).indexOf(1) === -1 ? -1 : arr.indexOf(1, idx)
+  // return arr.slice(idx).indexOf(1) === -1 ? -1 : arr.indexOf(1, idx)
+  return arr.indexOf(1, idx)
 }
