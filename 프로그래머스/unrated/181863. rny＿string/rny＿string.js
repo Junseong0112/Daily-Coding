6051fa0689d593a981b0e45b0b0dfc19bc5ab11a
Junseong <108931006+Junseong0112@users.noreply.github.com>
2023-11-02 23:27:49 +0900

    [level unrated] Title: rny_string, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.js
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.js
@@ -1,3 +1,5 @@
 function solution(rny_string) {
-    return [...rny_string].map((el) => el === 'm' ? 'rn' : el).join('')
+    // return [...rny_string].map((el) => el === 'm' ? 'rn' : el).join('')
+    // return rny_string.replaceAll('m', 'rn')
+    return rny_string.replace(/m/g, 'rn')
 }
