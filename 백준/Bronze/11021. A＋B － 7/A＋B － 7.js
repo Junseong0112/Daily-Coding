3d6d6c0104003722668e39afc134aedbcf54c401
Junseong <108931006+Junseong0112@users.noreply.github.com>
2023-11-11 20:13:40 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,11 @@
+const line = require("fs").readFileSync("dev/stdin", "utf8");
+let input = line.trim().split('\n')
+let numLength = parseInt(input[0])
+let answer = ''
+
+for(let i = 1; i <= numLength; i++){
+  let tmp = input[i].split(' ')
+  answer += `Case #${i}: `+ tmp.reduce((acc, cur) => acc + parseInt(cur), 0) + "\n"
+}
+
+console.log(answer)
