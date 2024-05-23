f3abab3de15beb5adf616ecba6ce6bf49de134ba
Junseong <108931006+Junseong0112@users.noreply.github.com>
2024-05-23 20:40:46 +0900

    [Bronze II] Title: 트럭 주차, Time: 116 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.js
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.js
@@ -1,45 +1,26 @@
-// const line = require("fs").readFileSync("/dev/stdin", "utf8");
 const fs = require("fs");
-const input = fs
-  .readFileSync("dev/stdin", "utf8")
-  .toString()
-  .trim()
-  .split("\n");
+const input = fs.readFileSync("/dev/stdin", "utf8").trim().split("\n");
 
-const price = input[0].split(" ").map(Number);
-const onePrice = price[0];
-const twoPrice = price[1];
-const threePrice = price[2];
+const [A, B, C] = input[0].split(" ").map(Number);
 
-let arr = [];
+let timeSlots = new Array(100).fill(0);
 
 for (let i = 1; i < input.length; i++) {
-  let num = input[i].split(" ").map(Number);
-  for (let i = num[0]; i < num[1]; i++) {
-    arr.push(i);
+  let [start, end] = input[i].split(" ").map(Number);
+  for (let j = start; j < end; j++) {
+    timeSlots[j]++;
   }
 }
-let sortArr = arr.sort((a, b) => a - b);
-let countArr = [];
-let count = 1;
 
-for (let i = 0; i < sortArr.length; i++) {
-  if (sortArr[i] == sortArr[i + 1]) {
-    count++;
-  } else {
-    countArr.push(count);
-    count = 1;
-  }
-}
 let totalPrice = 0;
 
-for (let i = 0; i < countArr.length; i++) {
-  if (countArr[i] === 1) {
-    totalPrice += onePrice;
-  } else if (countArr[i] === 2) {
-    totalPrice = totalPrice + twoPrice * 2;
-  } else if (countArr[i] === 3) {
-    totalPrice = totalPrice + threePrice * 3;
+for (let i = 0; i < timeSlots.length; i++) {
+  if (timeSlots[i] === 1) {
+    totalPrice += A;
+  } else if (timeSlots[i] === 2) {
+    totalPrice += B * 2;
+  } else if (timeSlots[i] === 3) {
+    totalPrice += C * 3;
   }
 }
 
